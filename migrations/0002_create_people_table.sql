3d65edc69dbafeef867780aca25cb1d14f00c83d
Vincent van Hoven <11269255+vincentvanhoven@users.noreply.github.com>
2025-12-21 19:17:07 +0100

    [ADD] DB migrations

---
+++ migrations/0002_create_people_table.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+CREATE TABLE people
+(
+    uuid            TEXT PRIMARY KEY,
+    sex             TEXT CHECK (sex IN ('male', 'female')),
+    firstname       TEXT,
+    lastname        TEXT,
+    birthdate       TEXT,
+    birthplace      TEXT,
+    family_uuid     TEXT,
+    profile_picture BLOB,
+    position_x      INTEGER,
+    position_y      INTEGER,
+    FOREIGN KEY (family_uuid) REFERENCES families (uuid) ON DELETE RESTRICT
+);
+
+-- +goose Down
+DROP TABLE people;
---
+++ migrations/0003_create_families_table.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE families
+(
+    uuid          TEXT PRIMARY KEY,
+    person_1_uuid TEXT,
+    person_2_uuid TEXT,
+    FOREIGN KEY (person_1_uuid) REFERENCES people (uuid) ON DELETE RESTRICT,
+    FOREIGN KEY (person_2_uuid) REFERENCES people (uuid) ON DELETE RESTRICT
+);
+
+-- +goose Down
+DROP TABLE families;
