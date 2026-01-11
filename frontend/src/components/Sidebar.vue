07e4fff530392e9ca481cf8bb10273a0e4f03361
Vincent van Hoven <11269255+vincentvanhoven@users.noreply.github.com>
2026-01-11 19:13:00 +0100

    [ADD] Remaining person basefields to Sidebar
    [CHANGE] Polished the Sidebar layout a bit.

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -5,26 +5,62 @@
         <hr class="mb-4" />
 
         <template v-if="selectedNode">
-            <div class="mb-4">ID #{{ selectedNode?.data.id }}</div>
-
-            <div class="mb-4">
+            <div class="mb-2">
                 <label for="firstname" class="font-semibold">First name</label>
                 <input
                     v-model="selectedNode.data.firstname"
                     name="firstname"
                     type="text"
-                    class="w-full bg-white border border-solid border-gray-400"
+                    class="w-full bg-white border border-solid border-gray-400
+                        px-1"
                 />
             </div>
 
-            <div class="mb-4">
+            <div class="mb-2">
                 <label for="lastname" class="font-semibold">Last name</label>
                 <input
                     v-model="selectedNode.data.lastname"
                     name="lastname"
                     type="text"
-                    class="w-full bg-white border border-solid border-gray-400"
+                    class="w-full bg-white border border-solid border-gray-400
+                        px-1"
                 />
+            </div>
+
+            <div class="mb-2">
+                <label for="sex" class="font-semibold">Sex</label>
+                <select
+                    v-model="selectedNode.data.sex"
+                    name="sex"
+                    class="w-full bg-white border border-solid border-gray-400 px-1"
+                >
+                    <option key="male" value="male">Male</option>
+                    <option key="female" value="female">Female</option>
+                </select>
+            </div>
+
+            <div class="mb-2">
+                <label for="birthdate" class="font-semibold">Date of birth</label>
+                <!-- :unmask="true" -->
+                <IMaskComponent
+                    v-model="selectedNode.data.birthdate"
+                    mask="0000-00-00"
+                    name="birthdate"
+                    type="text"
+                    placeholder="1950-12-01"
+                    class="w-full bg-white border border-solid border-gray-400
+                        px-1"
+                />
+            </div>
+
+            <div class="mb-2">
+                <label for="birthplace" class="font-semibold">Location of birth</label>
+                <input
+                    v-model="selectedNode.data.birthplace"
+                    name="birthplace"
+                    type="text"
+                    class="w-full bg-white border border-solid border-gray-400
+                        px-1" />
             </div>
         </template>
 
@@ -34,6 +70,8 @@
 
 <script lang="ts" setup>
     import { Node } from "@vue-flow/core";
+    import { onMounted, ref, watch } from "vue";
+    import { IMaskComponent } from "vue-imask";
 
     // Props
     const props = defineProps<{
